75cb06d3a5d706615bd5d6990118f02900b61cc3
Amith <amithomas95@gmail.com>
2019-10-30 12:38:48 +0530

    test trials

--- CloudFunction/main.py
+++ CloudFunction/main.py
@@ -13,33 +13,33 @@ dataflow = discovery.build('dataflow', 'v1b3', credentials=credentials)
 
 project = 'snappy-meridian-255502'
 def intitate_data_flow(data, context):
-	
-	jobName=data['name']
-  dbName= jobName.split(".")
-	tmpLocation = 'gs://df-temp-1/temp/'
-	templatePath ='gs://df-templates-1/templateDFtest.json'
-	fileLoc='gs://triggerbucket-1/'+jobName
-	
-	request_body = {
-          "environment": {
-            "zone": "us-central1-f",
-            "tempLocation": tmpLocation
-          },
-         "parameters": {
-           "inputFile": fileLoc,
-           "output" : dbName[0] 
-         },
-          "jobName": jobName
-        }
+
+jobName=data['name']
+dbName=jobName.split(".")
+tmpLocation = 'gs://df-temp-1/temp/'
+templatePath ='gs://df-templates-1/templateDFtest.json'
+fileLoc='gs://triggerbucket-1/'+jobName
+
+request_body = {
+        "environment": {
+          "zone": "us-central1-f",
+          "tempLocation": tmpLocation
+        },
+        "parameters": {
+          "inputFile": fileLoc,
+          "output" : dbName[0] 
+        },
+        "jobName": jobName
+      }
+
+request = dataflow.projects().templates().launch(
+      projectId=project,
+      body= request_body,
+      gcsPath = templatePath
+)
+
+response = request.execute()
+
+pprint(response)
   
-	request = dataflow.projects().templates().launch(
-        projectId=project,
-        body= request_body,
-        gcsPath = templatePath
-	)
 
-	response = request.execute()
-
-	pprint(response)
-		
-	
