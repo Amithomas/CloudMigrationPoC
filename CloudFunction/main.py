819ba00ac70bbdbc0fd9b4aab0de58a9c771b8f1
Amith <amithomas95@gmail.com>
2019-11-27 17:21:10 +0530

    dataflow tests

--- CloudFunction/main.py
+++ CloudFunction/main.py
@@ -19,7 +19,7 @@ def intitate_data_flow(data, context):
   name=data['name']
   dbName=name.split(".")
   ts = calendar.timegm(time.gmtime())
-  jobName = dbName[0] + "-" + ts
+  jobName = dbName[0] + "-" + str(ts)
   tmpLocation = 'gs://df-temp-1/temp/'
   templatePath ='gs://df-templates-1/templateJobStatDF.json'
   fileLoc='gs://triggerbucket-1/'+jobName
